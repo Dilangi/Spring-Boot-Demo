c46d8fc231bb655f64a156f6ac2decee6d9f8f17
Dilangi <dilangithilakarathne@gmail.com>
2025-07-02 10:50:59 +0100

    use of RequestParam, auto generate id, logging

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.UserDTO;
 import com.example.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value="api/v1/user")
 @CrossOrigin
+@Slf4j
 public class UserController {
     @Autowired
     private UserService userService;
@@ -33,9 +37,14 @@ public class UserController {
         return userService.deleteUser(userDTO);
     }
 
-    @GetMapping("/getUserById/{userId}")
-    public UserDTO getUserById(@PathVariable int userId){
-        return userService.getUserById(userId);
+    @GetMapping("/getUserById")
+    public ResponseEntity<UserDTO> getUserById(@RequestParam int id){
+        UserDTO user = userService.getUserById(id);
+        if (user == null) {
+            log.info("No such user with id"+id);
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping("/getUserByNameAndAge/{userName}/{userAge}")
